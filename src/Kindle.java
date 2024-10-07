51f06dc37d7c1a2bd3635a09831ffad3e2266b8c
brookebradley31 <brookebradley31@gmail.com>
2024-10-07 15:12:31 -0400

    Initial commit

---
+++ src/Kindle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Kindle {
+
+    int totalPages;
+    int currentPage;
+
+//================================================================================
+public Kindle(int totalPages) {
+    this.totalPages = totalPages;
+    currentPage = 1;
+}
+
+
+
+}
