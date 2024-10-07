48b25a70ed88a9d168ddf00ef24f039f1c5f7010
brookebradley31 <brookebradley31@gmail.com>
2024-10-07 15:36:59 -0400

    Version 2

--- src/Kindle.java
+++ src/Kindle.java
@@ -10,6 +10,24 @@ public Kindle(int totalPages) {
     currentPage = 1;
 }
 
+public String toString() {
+    return "Page " + currentPage + " of " + totalPages;
 
+}
 
+public void turnPages() {
+    currentPage = currentPage + 1;
+    if (currentPage > totalPages) {
+        currentPage = totalPages;
+        System.out.println("Turning a page would take you past the last page.");
+    }
+}
+
+public void turnPages(int n) {
+    currentPage = currentPage + n;
+    if (currentPage > totalPages) {
+        System.out.println("Turning " + n + " pages would take you past the last page.");
+        currentPage = totalPages;
+    }
+}
 }
